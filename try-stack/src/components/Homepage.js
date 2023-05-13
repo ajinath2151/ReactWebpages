43933a7a11a085defe01716411deaf4122cee9f8
ajinath <ajinath2151@gmail.com>
2023-05-13 12:15:48 +0530

    added background images and working in app bar

--- try-stack/src/components/Homepage.js
+++ try-stack/src/components/Homepage.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import '../../src/App.css';
+import GirlWithBagImg from '../images/background/GirlWithBag.jpg';
+import { AppBar, Button, Link, Toolbar } from '@material-ui/core';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
+const useStyles = makeStyles((theme)=>({
+  container: {    
+    backgroundImage: `url(${GirlWithBagImg})`,
+    backgroundSize: 'cover',     
+    height:570,    
+  },
+  }));
 
 function Homepage() {
+
+  const classes = useStyles();
+
   return (
-    <div>
-        Homepage
-    </div>
+    <>
+      <div className={classes.container}>
+        <AppBar className={classes.apBar}>
+          <Toolbar className={classes.toolBar}>            
+            <Button>Home</Button>
+            <Button>Home </Button>
+            <Button>Home</Button>
+            <Button>Home</Button>
+            <Button>Home <KeyboardArrowDown /></Button>
+            
+          </Toolbar>
+        </AppBar>
+      </div>  
+    </>
   )
 }
 
