8d441d726d21c992c0be6531c29815a58e14fe24
ajinath <ajinath2151@gmail.com>
2023-05-15 22:25:13 +0530

    complete design of app bar / navbar

--- try-stack/src/components/Homepage.js
+++ try-stack/src/components/Homepage.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import '../../src/App.css';
-import GirlWithBagImg from '../images/background/GirlWithBag.jpg';
-import { AppBar, Button, Link, Toolbar } from '@material-ui/core';
-import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
-const useStyles = makeStyles((theme)=>({
-  container: {    
-    backgroundImage: `url(${GirlWithBagImg})`,
-    backgroundSize: 'cover',     
-    height:570,    
-  },
-  }));
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../../src/App.css";
+import GirlWithBagImg from "../images/background/GirlWithBag.jpg";
+import { AppBar, Button, Link, Toolbar } from "@material-ui/core";
+import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
+
+const useStyles = makeStyles((theme) => ({
+  // container: {
+  //   backgroundImage: `url(${GirlWithBagImg})`,
+  //   backgroundSize: "cover",
+  //   height: 570,
+  // },
+  // item: {
+  //   "&:hover": {
+  //     backgroundColor: "red",
+  //   },
+  // },
+}));
 
 function Homepage() {
-
   const classes = useStyles();
 
   return (
     <>
-      <div className={classes.container}>
+      {/* <div className={classes.container}>
         <AppBar className={classes.apBar}>
           <Toolbar className={classes.toolBar}>            
-            <Button>Home</Button>
-            <Button>Home </Button>
-            <Button>Home</Button>
-            <Button>Home</Button>
-            <Button>Home <KeyboardArrowDown /></Button>
-   
+            <Button>Home <KeyboardArrowDown /> </Button>
+            <Button>Pages <KeyboardArrowDown />  </Button>
+            <Button>Block <KeyboardArrowDown /> </Button>
+            <Button className={classes.item}>Elements <KeyboardArrowDown /> </Button>              
           </Toolbar>
         </AppBar>
-      </div>  
+      </div>   */}
+      homepage
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
---
+++ try-stack/src/components/Navbar.js
@@ -0,0 +1,195 @@
+import { Button, Menu, MenuItem, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
+import { makeStyles } from "@material-ui/core/styles";
+import GirlWithBagImg from "../images/background/GirlWithBag.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundImage: `url(${GirlWithBagImg})`,
+    backgroundSize: "cover",
+    // width:"100%",
+    // height:"50%",
+    backgroundRepeat: "no-repeat",
+    height: 570,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  menuButtonHome: {
+    marginTop: 30,
+    color:"#c7c3c3",    
+    // marginLeft:200,
+    "&:hover": {
+      // backgroundColor: "red",
+      color: "white",
+    },
+  },
+  menuButtonPage: {
+    marginTop: 30,
+    color:"#c7c3c3",
+    "&:hover": {
+      // backgroundColor: "red",
+      color: "white",
+    },
+  },
+  menu: {
+    marginTop: 60,
+  },
+  stackText: {
+    marginTop: theme.spacing(3),
+    color: "white",
+    fontSize: 30,
+  },
+  appBarDiv: {
+    marginTop:theme.spacing(3),
+  },
+  tryBuilder: {
+    color:"white",
+    borderColor:"white",
+    borderRadius:"05px",
+  },
+  buyNow: {
+    backgroundColor:"blue",
+    color:"white",
+    marginLeft:theme.spacing(2),
+    borderRadius:"5px",
+    marginRight:theme.spacing(10),
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  const [homeAnchorEl, setHomeAnchorEl] = useState(null);
+  const [pagesAnchorEl, setPagesAnchorEl] = useState(null);
+
+  const handleHomeClick = (event) => {
+    setHomeAnchorEl(event.currentTarget);
+  };
+
+  const handleHomeClose = () => {
+    setHomeAnchorEl(null);
+  };
+
+  const handlePagesClick = (event) => {
+    setPagesAnchorEl(event.currentTarget);
+  };
+
+  const handlePagesClose = () => {
+    setPagesAnchorEl(null);
+  };
+
+  return (
+    <>
+      <div className={classes.container}>
+        <div>
+          <Button
+            aria-controls="home"
+            aria-haspopup="true"
+            onClick={handleHomeClick}
+            className={classes.menuButtonHome}
+            size="small"
+          >
+            Home
+            <KeyboardArrowDown />
+          </Button>
+
+          <Menu
+            id="home"
+            anchorEl={homeAnchorEl}
+            keepMounted
+            open={Boolean(homeAnchorEl)}
+            onClose={handleHomeClose}
+            className={classes.menu}
+          >
+            {/* appbar menu item inside          */}
+            <MenuItem onClick={handleHomeClose}>Profile</MenuItem>
+            <MenuItem onClick={handleHomeClose}>My Account</MenuItem>
+            <MenuItem onClick={handleHomeClose}>Logout</MenuItem>
+          </Menu>
+
+          <Button
+            aria-controls="pages"
+            aria-haspopup="true"
+            onClick={handlePagesClick}
+            className={classes.menuButtonPage}
+            size="small"
+          >
+            Pages
+            <KeyboardArrowDown />
+          </Button>
+
+          <Menu
+            id="pages"
+            anchorEl={pagesAnchorEl}
+            keepMounted
+            open={Boolean(pagesAnchorEl)}
+            onClose={handlePagesClose}
+            className={classes.menu}
+          >
+            {/* appbar menu item inside          */}
+            <MenuItem onClick={handlePagesClose}>Page 1</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 2</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 3</MenuItem>
+          </Menu>
+          <Button
+            aria-controls="pages"
+            aria-haspopup="true"
+            onClick={handlePagesClick}
+            className={classes.menuButtonPage}
+            size="small"
+          >
+            Blocks
+            <KeyboardArrowDown />
+          </Button>
+
+          <Menu
+            id="pages"
+            anchorEl={pagesAnchorEl}
+            keepMounted
+            open={Boolean(pagesAnchorEl)}
+            onClose={handlePagesClose}
+            className={classes.menu}
+          >
+            {/* appbar menu item inside          */}
+            <MenuItem onClick={handlePagesClose}>Page 1</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 2</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 3</MenuItem>
+          </Menu>
+          <Button
+            aria-controls="pages"
+            aria-haspopup="true"
+            onClick={handlePagesClick}
+            className={classes.menuButtonPage}
+            size="small"
+          >
+            Elements
+            <KeyboardArrowDown />
+          </Button>
+
+          <Menu
+            id="pages"
+            anchorEl={pagesAnchorEl}
+            keepMounted
+            open={Boolean(pagesAnchorEl)}
+            onClose={handlePagesClose}
+            className={classes.menu}
+          >
+            {/* appbar menu item inside          */}
+            <MenuItem onClick={handlePagesClose}>Page 1</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 2</MenuItem>
+            <MenuItem onClick={handlePagesClose}>Page 3</MenuItem>
+          </Menu>
+        </div>
+        <Typography className={classes.stackText}> <strong> stack </strong></Typography>
+        <div className={classes.appBarDiv}>
+          <Button className={classes.tryBuilder} variant="outlined"><strong> TRY BUILDER</strong></Button>
+          <Button className={classes.buyNow}><strong>BUY NOW</strong></Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
