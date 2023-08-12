62c8f07a934a404f28d3b7845eabb3e640286c36
Kiran <kiran.malave@inflooens.com>
2023-08-13 00:56:46 +0530

    create separate component for navbar,leftbar, feed and rightbar

--- mui/src/App.js
+++ mui/src/App.js
@@ -1,8 +1,24 @@
 
+import { Box, Stack } from '@mui/material';
+import './App.css';
+import Feed from './components/Feed';
+import Leftbar from './components/Leftbar';
+import Rightbar from './components/Rightbar';
+// import Navbar from './components/Navbar';
+
 function App() {
+
+
   return (
     <>
-    <div>Hello Ajinath shinde</div>
+    <Box>
+      {/* <Navbar/> */}
+      <Stack direction={'row'} spacing={2} justifyContent={'space-between'}>        
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>    
     </>
   );  
 }
