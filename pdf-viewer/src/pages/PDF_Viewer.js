829132998cc0bd83ef82ccc8a19f441976f6ef40
Ajinath Shinde <ajinath2151@gmail.com>
2023-10-28 00:40:53 +0530

    view/show pdf content done

--- pdf-viewer/src/pages/PDF_Viewer.js
+++ pdf-viewer/src/pages/PDF_Viewer.js
@@ -1,13 +1,65 @@
 import { Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import '../pages/PDF_Viewer.css';
+
+import { Viewer, Worker  } from "@react-pdf-viewer/core";
+import {defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PDF_Viewer = () => {
+
+  const [pdfFile, setPdfFile] = useState(null);
+  const [viewPdf, setViewPdf] = useState(null);
+
+  const handleOnChange = (event)=> {
+    const fileType = ['application/pdf']
+    let selectedFile = event.target.files[0];
+    if(selectedFile) {
+      if(selectedFile && fileType.includes(selectedFile.type)){
+        let reader = new FileReader()
+        reader.readAsDataURL(selectedFile);
+        reader.onload = (e) =>{
+          setPdfFile(e.target.result);
+        } 
+      }
+      else {
+        setPdfFile(null)
+      }
+    }
+    else {
+      console.log("please select file");
+    }
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(pdfFile!== null) {
+      setViewPdf(pdfFile)      
+    }
+    else {
+      setViewPdf(null)
+    }
+  }
+  const newplugin = (defaultLayoutPlugin);
   return (
-    <div>
+    <>
       <h1>PDF Viewer..</h1>      
-      <TextField id="outlined-basic" label="" variant="outlined" type='file' /> <br />
-      <Button type='submit' variant='outlined'>View PDF</Button>
-    </div>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+        <TextField id="outlined-basic" label="" variant="outlined" type='file' onChange={handleOnChange} /> <br />
+        <Button type='submit' variant='outlined' onClick={handleSubmit}>View PDF</Button>
+        </form>      
+        <div className="pdfContainer">
+          <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+            {viewPdf && <>
+              <Viewer fileUrl={viewPdf} plugins={[newplugin]} />
+            </>}
+            {!viewPdf && <>No PDF</>}
+          </Worker>        
+        </div>
+      </div>
+    </>
   )
 }
 
